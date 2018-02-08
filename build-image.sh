09df05b056de89cab0b43ed256e729efcf80cafe
Tyson L. Swetnam <tyson-swetnam@users.noreply.github.com>
2018-02-08 08:28:35 -0700

    Update build-image.sh

--- build-image.sh
+++ build-image.sh
@@ -10,4 +10,4 @@ BASENAME="eemt-v$TS"
 
 rm -f $BASENAME.img
 singularity create --size 4000 $BASENAME.img
-singularity bootstrap $BASENAME.img Singularity
+singularity bootstrap $BASENAME.simg Singularity
